def23d06208da90dd5085f3769d0e95ff4edabbf
ichigatsu <firstmoon1986@gmail.com>
2025-02-04 18:10:22 +0900

    初回アップロード: HTML & CSS & JS 更新

--- static/script.js
+++ static/script.js
@@ -2,26 +2,26 @@ const hamBtnOpen = document.querySelector('#hamburger-btn-open')
 const hamBtnClose = document.querySelector('#hamburger-btn-close')
 const hamMenu = document.querySelector('#hamburger-menu')
 const menuOpen = hamBtnOpen.addEventListener('click', () => {
-    console.log('ボタンが押されました')
     hamMenu.animate(
         {
             translate: ['0 -50vh', 0]
         },
         {
             duration: 800,
-            fill:"forwards"
+            fill:"forwards",
+            easing: 'ease-out',
         },
     );
 });
 const menuClose = hamBtnClose.addEventListener('click', () => {
-    console.log('ボタンが押されました')
     hamMenu.animate(
         {
             translate: [0, '0 -50vh']
         },
         {
             duration: 800,
-            fill:"forwards"
+            fill:"forwards",
+            easing: 'ease-out',
         },
     );
 });
